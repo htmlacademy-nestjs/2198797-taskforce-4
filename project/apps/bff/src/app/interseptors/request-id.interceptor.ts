187fc11362c56ae252eac08f2cef1101283e85c4
Vitalii Beliaev <vtaliy.belyaev@gmail.com>
2023-09-04 17:59:20 +0300

    Исправил тип any

--- project/apps/bff/src/app/interseptors/request-id.interceptor.ts
+++ project/apps/bff/src/app/interseptors/request-id.interceptor.ts
@@ -1,9 +1,8 @@
 import { CallHandler, ExecutionContext, Logger, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
 import * as crypto from 'crypto';
 
 export class RequestIdInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+  intercept(context: ExecutionContext, next: CallHandler) {
     const requestId = crypto.randomUUID();
     const request = context.switchToHttp().getRequest<Request>();
     request.headers['X-Request-Id'] = requestId;
